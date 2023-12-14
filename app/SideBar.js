dec691ce591ea593a3c17e936ae4369728c425ea
Riya Dev <riyasdev9@gmail.com>
2023-12-14 18:57:29 -0500

    basic tailwind sidebar component

---
+++ app/SideBar.js
@@ -0,0 +1,21 @@
+const SideBar = () => {
+    return (
+        <div className="fixed top-0 left-0 h-screen w-16 m-0
+                        flex flex-col 
+                        bg-primary text-white shadow-lg">
+            <i>A</i>
+            <i>B</i>
+            <i>C</i>
+            <i>D</i>
+            <i>E</i>
+        </div>
+    );
+};
+
+const SideBarIcon = ({icon}) => (
+    <div className="sidebar-icon">
+        {icon}
+    </div>
+);
+
+export default SideBar;
