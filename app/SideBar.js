15557168d38c82c33f13e2565dd405d58bc0b0db
Riya Dev <riyasdev9@gmail.com>
2023-12-15 11:49:47 -0500

    completed 1st draft of sidebar

--- app/SideBar.js
+++ app/SideBar.js
@@ -1,20 +1,32 @@
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { RiGraduationCapFill, RiGraduationCapLine, RiGithubFill, RiGithubLine, 
+        RiCodeBoxFill, RiCodeBoxLine, RiMicFill, RiMicLine, RiMickeyFill, RiMickeyLine } from 'react-icons/ri';
+
 const SideBar = () => {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0
                         flex flex-col 
                         bg-primary text-white shadow-lg">
-            <i>A</i>
+            {/* <i>A</i>
             <i>B</i>
             <i>C</i>
             <i>D</i>
-            <i>E</i>
+            <i>E</i> */}
+            <SideBarIcon icon={<RiMickeyFill size="28"/>} text={"Home"} /> {/*home page*/}
+            <SideBarIcon icon={<RiGraduationCapFill size="28"/>} text={"Education"} /> {/*education page*/}
+            <SideBarIcon icon={<RiCodeBoxFill size="28"/>} text={"Projects"} /> {/*project page*/}
+            <SideBarIcon icon={<RiGithubFill size="28"/>} text={"Links"} /> {/*links page*/}
+            
         </div>
     );
 };
 
-const SideBarIcon = ({icon}) => (
-    <div className="sidebar-icon">
+const SideBarIcon = ({icon, text='tooltip'}) => (
+    <div className="sidebar-icon group">
         {icon}
+        <span class="sidebar-tooltip group-hover:scale-100">
+            {text}
+        </span>
     </div>
 );
 
