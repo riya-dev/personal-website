5cee28267763e2fe008c72807f5753327cc4d3b2
Riya Dev <riyasdev9@gmail.com>
2023-12-24 16:23:48 -0500

    copyright

--- app/BottomNav.js
+++ app/BottomNav.js
@@ -2,8 +2,9 @@ const BottomNav = () => {
     return (
         <>
             <div className="flex flex-row bottom-16 left-0 h-16 w-screen m-0
-                            items-center
+                            items-center justify-center text-center
                             bg-purple-900 text-white shadow-lg">
+                <h2>© Riya Dev 2023</h2>
             </div>
         </>
     );
