5322cbfce4b8eff479eb1666862f2e657edc7d46
Riya Dev <riyasdev9@gmail.com>
2023-12-24 11:49:19 -0500

    projects section updates

--- app/contentComponents/Projects.js
+++ app/contentComponents/Projects.js
@@ -2,6 +2,8 @@ import '../styles/projects.css'
 import Image from 'next/image'
 import this_website from '/public/static/images/projects/this_website.png'
 import fitted from '/public/static/images/projects/fitted.png'
+import rotating_platonic_solid from '/public/static/images/projects/rotating_platonic_solid.gif'
+
 
 const Projects = () => {
     return(
@@ -12,7 +14,7 @@ const Projects = () => {
 
                     <section class="projects-grid">
                         <section class="project-card">
-                            <h2>This Website!</h2>
+                            <h2>This Website</h2>
                             <Image src={this_website}></Image>
                             <h3>Next.js • React • Tailwind CSS • HTML • CSS • Vercel</h3>
                         </section>
@@ -24,11 +26,15 @@ const Projects = () => {
                         </section>
 
                         <section class="project-card">
-                            <h1>AI</h1>
+                            <h2>AI Othello Game + Solving Bot</h2>
+                            {/* <Image src={fitted}></Image> */}
+                            <h3>Python •  </h3>
                         </section>
 
                         <section class="project-card">
-                            <h1>CV</h1>
+                            <h2>CV 3D Platonic Rotating Solid</h2>
+                            <Image src={rotating_platonic_solid}></Image>
+                            <h3>OpenCV • C++ </h3>
                         </section>
                     </section>
                 </section>
