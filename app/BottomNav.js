44a4325d2799610413693741ed790a6466aa6b15
Riya Dev <riyasdev9@gmail.com>
2024-01-02 08:53:44 -0500

    Updated footer to 2024.

--- app/BottomNav.js
+++ app/BottomNav.js
@@ -4,7 +4,7 @@ const BottomNav = () => {
             <div className="flex flex-row bottom-16 left-0 h-16 w-screen m-0
                             items-center justify-center text-center
                             bg-purple-900 text-white shadow-lg">
-                <h2>© Riya Dev 2023</h2>
+                <h2>© Riya Dev 2024</h2>
             </div>
         </>
     );
