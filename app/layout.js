29f46ec69223f72d48cea6dd1c2ee5d9125719fb
Riya Dev <riyasdev9@gmail.com>
2024-01-06 16:26:30 -0500

    Added meta data tags.

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
+import Head from 'next/head';
 import { Inter, Poppins } from 'next/font/google'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+// import { useRouter } from 'next/router'
 
 const inter = Inter({ subsets: ['latin'] })
 const poppins = Poppins({
@@ -13,19 +15,44 @@ const poppins = Poppins({
 
 export const metadata = {
   title: 'Riya Dev',
-  description: 'Riya Dev personal website',
+  author: 'Riya Dev',
+  applicationName: 'Riya Dev personal website.',
+  keywords: ['Riya', 'Dev', 'TJHSST', 'Michigan', 'Computer', 'Singing', 'Personal Portfolio', 'Website'],
+  description: 'Riya Dev personal website. I am passionate about innovation and impact with a background in public speaking/varsity debate, computer science, start-ups, and research.',
   siteRepo: 'https://github.com/riya-dev/personal-website',
-  siteLogo: '/public/static/images/favicon.ico',
+  siteLogo: '/static/images/favicon.ico',
+  language: 'en-US',
+  siteURL: 'https://riyadev.com/'
 }
 
 export default function RootLayout({ children }) {
+  // const router = useRouter();
+
   return (
-    <html lang="en">
-      <body className={inter.className + ' ' + poppins.className}>
-        {children}
-        <Analytics />
-        <SpeedInsights />
-      </body>
-    </html>
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="author" content={metadata.author} />
+        <meta name="applicationName" content={metadata.applicationName} />
+        <meta name="keywords" content={metadata.keywords.join(', ')} />
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href={metadata.siteLogo} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/static/images/projects/this_website.png" />
+        <meta property="og:url" content={metadata.siteURL} />
+        <meta name="twitter:site" content="@ceoRiya" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <link rel="canonical" href={metadata.siteURL} />
+      </Head>
+
+      <html lang={metadata.language}>
+        <body className={inter.className + ' ' + poppins.className}>
+          {children}
+          <Analytics />
+          <SpeedInsights />
+        </body>
+      </html>
+    </>
   )
 }
