559c8248f00c13186f4141e01ec54d9081489a28
Riya Dev <riyasdev9@gmail.com>
2024-09-17 12:36:41 -0400

    Edited aesthetics of about section and added hover animation to bouncing arrow.

--- app/contentComponents/About.js
+++ app/contentComponents/About.js
@@ -6,13 +6,14 @@ const About = () => {
       <>
         <div className="about-container" id="about">
           <section className="about-section">
-            <h1 className="text-purple-900 overscroll-behavior:contain">
+            <h1 className="text-purple-900 text-5xl font-extrabold mb-4">
               About Me
             </h1>
             <div className="about-description">
-              <p className="tracking-wider">I am Riya Dev.</p>
-              {/* text-purple-200 */}
-              <p className="tracking-tight font-normal">
+              <p className="tracking-wider text-lg font-semibold">
+                I am Riya Dev.
+              </p>
+              <p className="tracking-tight font-normal text-gray-700 mb-4">
                 I've been coding since the age of 9 when I wrote my first
                 JavaScript program — a Samsung Galaxy S4 clone. Intrigued by the
                 boundless possibilities of code, I pursued an Artificial
@@ -25,7 +26,7 @@ const About = () => {
                 technological toolkit to kickstart more projects and build
                 impactful innovations.
               </p>
-              <p className="tracking-tight font-normal">
+              <p className="tracking-tight font-normal text-gray-800 mb-4">
                 When I'm not coding, you can find me singing to Adele, gushing
                 over the latest fashion shows, listening to my favorite podcasts
                 (Morning Brew Daily and How I Built This are my favorites), or
@@ -35,7 +36,10 @@ const About = () => {
 
             <div className="arrow bounce">
               <a href="#education" alt="Scroll to education section">
-                <RiArrowDropDownLine size="72" className="text-purple-900" />
+                <RiArrowDropDownLine
+                  size="72"
+                  className="text-purple-900 hover:text-purple-600 transition duration-300"
+                />
               </a>
             </div>
           </section>
