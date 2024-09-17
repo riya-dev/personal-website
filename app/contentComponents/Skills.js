d43ea690299eb72e203dc070f32e59f373d56fe6
Riya Dev <riyasdev9@gmail.com>
2024-09-17 12:46:18 -0400

    Cleaned code. Removed redundant closing brackets.

--- app/contentComponents/Skills.js
+++ app/contentComponents/Skills.js
@@ -1,19 +1,17 @@
 import "../styles/skills.css";
-import * as languagesData from "../json/languages.json";
+import languagesData from "../json/languages.json";
+import frameworksData from "../json/frameworks.json";
+import librariesData from "../json/libraries.json";
+import developerToolsData from "../json/developer_tools.json";
+
 const languages = languagesData.languages;
-
-import * as frameworksData from "../json/frameworks.json";
 const frameworks = frameworksData.frameworks;
-
-import * as librariesData from "../json/libraries.json";
 const libraries = librariesData.libraries;
-
-import * as developerToolsData from "../json/developer_tools.json";
 const developerTools = developerToolsData.developer_tools;
 
 const SkillSection = ({ iconClass, name }) => (
   <div className="skill-item">
-    <i className={iconClass}></i>
+    <i className={iconClass} />
     <h3 className={name.length > 10 ? "small-font" : ""}>{name}</h3>
   </div>
 );
@@ -24,13 +22,16 @@ const Skills = () => {
       <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
-      ></link>
-      <div className="skills-container" id="skills">
+      />
+      <div
+        className="skills-container bg-gradient-to-b from-purple-100 to-white"
+        id="skills"
+      >
         <section className="skills-section">
           <h1 className="text-purple-900">Skills</h1>
           <section className="skills-grid md:grid md:grid-cols-4 md:gap-4">
             {/* Languages */}
-            <section className="skills-card">
+            <section className="skills-card transition-transform duration-300 hover:scale-105">
               <h2>Languages</h2>
               <div className="grid grid-cols-3 gap-2">
                 {languages.map((language, index) => (
@@ -44,7 +45,7 @@ const Skills = () => {
             </section>
 
             {/* Frameworks */}
-            <section className="skills-card">
+            <section className="skills-card transition-transform duration-300 hover:scale-105">
               <h2>Frameworks</h2>
               <div className="grid grid-cols-3 gap-2">
                 {frameworks.map((framework, index) => (
@@ -58,7 +59,7 @@ const Skills = () => {
             </section>
 
             {/* Libraries */}
-            <section className="skills-card">
+            <section className="skills-card transition-transform duration-300 hover:scale-105">
               <h2>Libraries</h2>
               <div className="grid grid-cols-3 gap-2">
                 {libraries.map((library, index) => (
@@ -72,7 +73,7 @@ const Skills = () => {
             </section>
 
             {/* Developer Tools */}
-            <section className="skills-card">
+            <section className="skills-card transition-transform duration-300 hover:scale-105">
               <h2>Developer Tools</h2>
               <div className="grid grid-cols-3 gap-2">
                 {developerTools.map((tool, index) => (
