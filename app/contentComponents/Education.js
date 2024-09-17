e1a071c27710882bb5ba7ca1f6fd72c3948c1ac9
Riya Dev <riyasdev9@gmail.com>
2024-09-17 15:39:22 -0400

    Optimize sizing for small and medium screens. Formatting edits to color and margins.

--- app/contentComponents/Education.js
+++ app/contentComponents/Education.js
@@ -6,19 +6,19 @@ import tjhsst from '/public/static/images/tjhsst.png'
 const Education = () => {
     return (
       <div
-        className="education-container bg-gradient-to-b from-purple-100 to-white flex align-middle justify-center items-center min-h-screen m-0 auto"
+        // className="education-container bg-gradient-to-b from-purple-100 to-white flex align-middle justify-center items-center min-h-screen m-0 auto"
+        className="bg-gradient-to-b from-purple-100 to-white flex justify-center items-center min-h-[90vh] sm:min-h-screen m-0"
         id="education"
       >
-        <section className="education-section max-w-6xl mx-auto px-6 text-center mb-20">
-          <h1 className="text-purple-900 text-4xl font-bold text-center mb-4 auto">
+        <section className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
+          <h1 className="text-purple-900 text-5xl font-bold mb-12 auto">
             Education
           </h1>
-
           {/* Education grid */}
           <div className="grid md:grid-cols-2 gap-12 justify-items-center">
             {/* University of Michigan Card */}
             <div className="education-card bg-white rounded-lg shadow-lg overflow-hidden transition-transform duration-300 hover:scale-105">
-              <div className="bg-purple-100 p-6 flex items-center justify-center">
+              <div className="bg-purple-200 p-6 flex items-center justify-center">
                 <a
                   href="https://cse.engin.umich.edu/news/"
                   alt="Riya Dev University of Michigan College of Engineering"
@@ -52,7 +52,7 @@ const Education = () => {
 
             {/* TJHSST Card */}
             <div className="education-card bg-white rounded-lg shadow-lg overflow-hidden transition-transform duration-300 hover:scale-105">
-              <div className="bg-purple-100 p-6 flex items-center justify-center">
+              <div className="bg-purple-200 p-6 flex items-center justify-center">
                 <a
                   href="https://en.wikipedia.org/wiki/Thomas_Jefferson_High_School_for_Science_and_Technology"
                   alt="Riya Dev Thomas Jefferson High School for Science and Technology (TJHSST) Alum"
