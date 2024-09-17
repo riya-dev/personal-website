0609a502ba0ba801cbc2dac35267d7111bc86508
Riya Dev <riyasdev9@gmail.com>
2024-09-17 12:37:07 -0400

    Added hover color animation to bouncing arrow.

--- app/contentComponents/Home.js
+++ app/contentComponents/Home.js
@@ -1,40 +1,44 @@
-import Image from 'next/image'
-import '../styles/home.css'
-import Riya from '/public/static/images/pfp.jpg'
-import { RiArrowDropDownLine } from 'react-icons/ri';
+import Image from "next/image";
+import "../styles/home.css";
+import Riya from "/public/static/images/pfp.jpg";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 const Home = () => {
-    return(
-        <>
-            <div className="home-container" id="home">
-                <section className="home-section">
-                    {/* <Image 
+  return (
+    <>
+      <div className="home-container" id="home">
+        <section className="home-section">
+          {/* <Image 
                         src={Riya}
                         alt="Picture of Riya Dev in from of laptop with TJHSST sticker."
                         draggable="false"
                         className="home-image rounded-full
                                     hover:scale-110 transition-all duration-500" />  */}
-                    <div className="home-image-div"
-                        alt="Picture of Riya Dev using laptop with TJHSST sticker."></div>
-                    {/* add animation when clicking. explode confetti or music notes or something */}
+          <div
+            className="home-image-div"
+            alt="Picture of Riya Dev using laptop with TJHSST sticker."
+          ></div>
+          {/* add animation when clicking. explode confetti or music notes or something */}
 
-                    <h1 className="highlight text-purple-900">
-                        Riya Dev
-                    </h1>
+          <h1 className="highlight text-purple-900">Riya Dev</h1>
 
-                    {/* 리야 데브 */}
-                    <h2 className="text-purple-900">Software Engineer • Innovator • Singer</h2>
+          {/* 리야 데브 */}
+          <h2 className="text-purple-900">
+            Software Engineer • Innovator • Singer
+          </h2>
 
-                    <div className="arrow bounce">
-                        <a href="#about"
-                            alt="Scroll to about section">
-                            <RiArrowDropDownLine size="72" className="text-purple-900" />
-                        </a>
-                    </div>
-                </section>
-            </div>
-        </>
-    )
-}
+          <div className="arrow bounce">
+            <a href="#about" alt="Scroll to about section">
+              <RiArrowDropDownLine
+                size="72"
+                className="text-purple-900 hover:text-purple-600 transition duration-300"
+              />
+            </a>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
